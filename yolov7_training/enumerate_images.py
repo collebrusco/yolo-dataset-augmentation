1990aa8986eb3ac7f6bb9d819251f2366ea98676
collebrusco <collebrusco.frank@gmail.com>
2023-11-14 18:27:10 -0600

    get working colab configuration. Takes some massaging to get it to work

--- yolov7_training/enumerate_images.py
+++ yolov7_training/enumerate_images.py
@@ -5,10 +5,10 @@ def list_filenames(directory_path, output_file_name):
 
     with open(output_file_name, 'w') as output_file:
         for file_name in file_names:
-            output_file.write(file_name + '\n')
+            output_file.write('./' + directory_path + '/' + file_name + '\n')
 
 
 if __name__ == '__main__':
-	list_filenames('Images/Train', 'train.txt')
-	list_filenames('Images/Test', 'test.txt')
-	list_filenames('Images/Validation', 'val.txt')
+	list_filenames('images/Train', 'train.txt')
+	list_filenames('images/Test', 'test.txt')
+	list_filenames('images/Validation', 'val.txt')
--- yolov7_training/train.sh
+++
@@ -1,3 +0,0 @@
-python train.py --workers 8 --device 0 --batch-size 32 \
---data base.yaml --img 512 512 --cfg cfg/training/yolov7-custom.yaml \
---weights 'yolov7_training.pt' --name yolov7-custom --hyp data/hyp.scratch.custom.yaml
